4dc5225bd254b3e557005bdaeb4b6094d568a85d
Bubbus <splambob@googlemail.com>
2013-07-04 21:07:00 +0100

    XCF Models now auto-download to clients.  Delete xcf_modelincludes.lua to disable.

---
+++ lua/autorun/server/xcf_modelincludes.lua
@@ -0,0 +1,22 @@
+if not SERVER then return end
+
+// FFARs
+resource.AddFile("models/missiles/ffar_40mm.mdl")
+resource.AddFile("materials/models/missiles/launcher7_40mm.vmt")
+resource.AddFile("models/missiles/hydra40_ambient.vtf")
+resource.AddFile("models/missiles/hydra40_norm.vtf")
+
+resource.AddFile("models/missiles/ffar_40mm_closed.mdl")
+
+resource.AddFile("models/missiles/ffar_70mm.mdl")
+resource.AddFile("models/missiles/ffar_70mm_closed.mdl")
+
+
+// Racks
+resource.AddFile("models/missiles/launcher7_40mm.mdl")
+resource.AddFile("materials/models/missiles/launcher7_40mm.vmt")
+resource.AddFile("materials/models/missiles/launcher7_40mm_dull.vmt")
+resource.AddFile("models/missiles/launcher7_40mm_skin1.vtf")
+resource.AddFile("models/missiles/launcher7_40mm_norm.vtf")
+
+resource.AddFile("models/missiles/launcher7_70mm.mdl")
