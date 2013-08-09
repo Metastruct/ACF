bb4881003032aa0d3a45ca5a047d3d1dbbddff0d
Bubbus <splambob@googlemail.com>
2013-08-10 00:47:10 +0100

    XCF Supporting Content is now on the workshop!  Changed xcf_modelincludes.lua to use it.

--- lua/autorun/server/xcf_modelincludes.lua
+++ lua/autorun/server/xcf_modelincludes.lua
@@ -1,6 +1,7 @@
 if not SERVER then return end
 
-// FFARs
+--[[
+-- FFARs
 resource.AddFile("models/missiles/ffar_40mm.mdl")
 resource.AddSingleFile("materials/models/missiles/launcher7_40mm.vmt")
 resource.AddSingleFile("models/missiles/hydra40_ambient.vtf")
@@ -12,7 +13,7 @@ resource.AddFile("models/missiles/ffar_70mm.mdl")
 resource.AddFile("models/missiles/ffar_70mm_closed.mdl")
 
 
-// Racks
+-- Racks
 resource.AddFile("models/missiles/launcher7_40mm.mdl")
 resource.AddSingleFile("materials/models/missiles/launcher7_40mm.vmt")
 resource.AddSingleFile("materials/models/missiles/launcher7_40mm_dull.vmt")
@@ -20,5 +21,7 @@ resource.AddSingleFile("models/missiles/launcher7_40mm_skin1.vtf")
 resource.AddSingleFile("models/missiles/launcher7_40mm_norm.vtf")
 
 resource.AddFile("models/missiles/launcher7_70mm.mdl")
+]]--
+resource.AddWorkshop("168183029")	-- woot!
 
 print("XCF client resources added to download list.")
